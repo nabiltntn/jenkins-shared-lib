cf7976ace4eec631206fd683e66fe069e4e8d12c
nabiltntn <zouabi.nabil@live.com>
2022-04-10 02:41:22 +0200

    Fix

--- vars/loadLinuxScript.groovy
+++ vars/loadLinuxScript.groovy
@@ -1,5 +1,5 @@
 def call(Map config = [:]) {
-  def scriptContent = libraryResource "com/nz/scripts/linux/${config.name}
-  writeFile file : "./${config.name}", text: ${scriptContent}
+  def scriptContent = libraryResource "com/nz/scripts/linux/${config.name}"
+  writeFile file : "./${config.name}", text: scriptContent
   sh "chmod a+x ./${config.name}"
 }
